7c8db140944ffdd4bfc239bd90e2e868686359e6
YasminaTaha93 <yasminataha93@gmail.com>
2023-10-05 10:15:06 -0700

    +

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'prints specific text'
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+	char str1[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str1);
+	return (0);
+}
