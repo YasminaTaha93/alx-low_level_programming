c962ec4e3955c7e4449f59a2ded61a57bb754418
YasminaTaha93 <yasminataha93@gmail.com>
2023-10-19 05:50:59 -0700

    -

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include "_strlen.c"
+
+/**
+ * _strcat - entry point
+ * Description: appends src to dest
+ * @src: user input
+ * @dest: user destination
+ * Return: string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i >= 0)
+	{
+		if (dest[i] != '\0')
+		{
+			j++;
+			i++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	for (i = j; i < _strlen(src) + j; i++)
+	{
+		dest[i] = src[i - j];
+	}
+	return (dest);
+}
