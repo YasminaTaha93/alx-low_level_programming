689859cbe2cbd64ac6fc9bb428b53fe388ee616b
YasminaTaha93 <yasminataha93@gmail.com>
2023-10-23 15:56:56 -0700

    -

---
+++ 0x07-pointers_arrays_strings/xstrlen.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strlen - entry point
+ * Description: calculate string length
+ * @a: user input
+ * Return: integer
+*/
+
+int _strlen(char *a)
+{
+        int i = 0;
+        int n;
+
+        while (i >= 0)
+        {
+                if (a[i] != '\0')
+                        i++;
+                else
+                {
+                        n = i;
+                        break;
+                }
+        }
+        return (n);
+}
